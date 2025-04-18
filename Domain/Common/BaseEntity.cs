bdaef8b5f6bd5f7871ee445eea9cd967031a799a
Mohammad Mahdi <mahdikhoubrouy@gmail.com>
2025-04-18 23:00:06 +0330

    Add project files.

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime DateCreate { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Common;
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+
+        public UserRole Role { get; set; } = UserRole.Customer;
+    }
+}
